e62eed72c5959c26ccba8dde1a096fcadb8a5eb4
Gleb Gerasimov <glebol2017@gmail.com>
2026-04-21 12:16:43 +0300

    update EncryptionManager.h

--- include/EncryptionManager.h
+++ include/EncryptionManager.h
@@ -5,12 +5,24 @@
 #ifndef SECURE_TOURISM_DB_SYSTEM_ENCRYPTIONMANAGER_H
 #define SECURE_TOURISM_DB_SYSTEM_ENCRYPTIONMANAGER_H
 
-
+#include <QString>
+#include <QByteArray>
 
 class EncryptionManager {
+public:
+    EncryptionManager();
+    ~EncryptionManager();
 
+    [[nodiscard]] QByteArray encrypt(const QString& plainText) const;
+    [[nodiscard]] QString decrypt(const QByteArray& data) const;
+    [[nodiscard]] QString hash(const QString& data) const;
+
+private:
+    QByteArray m_key;
+
+    [[nodiscard]] QByteArray generateIV() const;
+
+    void initialize();
 };
 
-
-
-#endif //SECURE_TOURISM_DB_SYSTEM_ENCRYPTIONMANAGER_H
+#endif // SECURE_TOURISM_DB_SYSTEM_ENCRYPTIONMANAGER_H
