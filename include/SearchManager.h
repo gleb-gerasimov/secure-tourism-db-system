10158727617b7ca0be48ab1ac39c5cfc57226748
Gleb Gerasimov <glebol2017@gmail.com>
2026-04-21 11:57:52 +0300

    update SearchManager.h

--- include/SearchManager.h
+++ include/SearchManager.h
@@ -4,11 +4,22 @@
 
 #ifndef SECURE_TOURISM_DB_SYSTEM_SEARCHMANAGER_H
 #define SECURE_TOURISM_DB_SYSTEM_SEARCHMANAGER_H
-
+#include "Record.h"
+#include "Database.h"
+#include "EncryptionManager.h"
 
 
 class SearchManager {
+public:
+    SearchManager(Database* database, EncryptionManager* encryptionManager);
 
+    [[nodiscard]] QVector<Record> findByDateRange(const QString& start, const QString& end) const;
+    [[nodiscard]] QVector<Record> findByType(const QString& type) const;
+    [[nodiscard]] QVector<Record> findByCountry(const QString& country) const;
+
+private:
+    Database* m_database;
+    EncryptionManager* m_encryptionManager;
 };
 
 
