150f3b09284607708e8eb6674a5ac216f280d716
Gleb Gerasimov <glebol2017@gmail.com>
2026-04-26 21:02:51 +0300

    Implement Controller.cpp logic

--- src/Controller.cpp
+++ src/Controller.cpp
@@ -3,3 +3,45 @@
 //
 
 #include "../include/Controller.h"
+#include "Database.h"
+#include "SearchManager.h"
+
+
+Controller::Controller(Database* database, SearchManager* searchManager)
+    : m_database(database),
+      m_searchManager(searchManager) {
+}
+
+QVector<Record> Controller::getAllRecords() const {
+    return m_database->getAllRecords();
+}
+
+QVector<Record> Controller::findTours(
+    const std::optional<QDate>& startDate,
+    const std::optional<QDate>& endDate,
+    const std::optional<QString>& country,
+    const std::optional<QString>& type,
+    const std::optional<bool>& hasFlight,
+    const std::optional<int>& participants,
+    const std::optional<double>& price
+) const {
+    return m_searchManager->findTours(
+        startDate,
+        endDate,
+        country,
+        type,
+        hasFlight,
+        participants,
+        price
+    );
+}
+
+double Controller::getAverageDailyCost(
+    const QString& country,
+    const QString& type
+) const {
+    return m_searchManager->getAverageDailyCost(
+        country,
+        type
+    );
+}
